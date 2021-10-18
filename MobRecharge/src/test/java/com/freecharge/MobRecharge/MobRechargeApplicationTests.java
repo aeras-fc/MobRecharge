3e22e9a3214f47b9c5f7b243884bf92dc32ddb64
aeras.alvi <aeras.alvi@L-8BFHV93.ad.fc.net>
2021-10-18 10:25:08 +0530

    Initial commit!

---
+++ MobRecharge/src/test/java/com/freecharge/MobRecharge/MobRechargeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freecharge.MobRecharge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobRechargeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
