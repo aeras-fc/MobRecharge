515298cf16a9cd84cee1427050490ad9f72e74ac
darshdeep2018 <43698990+darshdeep2018@users.noreply.github.com>
2021-10-18 12:09:50 +0530

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,25 @@
+package com.freecharge.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class BankAccount {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer bid;
+	
+	String bankName;
+	String accountHolderName;
+	String accountNumber;
+	String createdDate;
+	String updatedDate;
+	double balance;
+	
+
+
+}
